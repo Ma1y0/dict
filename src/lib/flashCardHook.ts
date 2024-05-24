c3859b63d96f153c84d916e759f4e90f32b4e6de
Ma1y0 <matyas.barr@gmail.com>
2024-05-24 08:33:09 +0200

    Fix: useCardId hook out of bound

--- src/lib/flashCardHook.ts
+++ src/lib/flashCardHook.ts
@@ -3,14 +3,13 @@ import { useState } from "react";
 export function useCardId(max: number) {
   const [cardId, setCardId] = useState<number>(0);
 
-
   const nextCard = () => {
-    if (cardId < max) setCardId((prevState) => prevState + 1);
-		console.log(cardId + 1)
+    if (cardId < max - 1) setCardId((prevState) => prevState + 1);
+    console.log(cardId + 1);
   };
   const prevCard = () => {
     if (cardId > 0) setCardId((prevState) => prevState - 1);
-		console.log(cardId - 1)
+    console.log(cardId - 1);
   };
 
   return { cardId, nextCard, prevCard };
